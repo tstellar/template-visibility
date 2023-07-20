2f3029342dbd6b397da0e330de0d5283942f14bb
Tom Stellard <tstellar@redhat.com>
2023-07-19 17:39:35 -0700

    Change order of template instantiation

--- templates.cpp
+++ templates.cpp
@@ -2,12 +2,18 @@
 #include "templates.h"
 
 
-/* clang 16.0.5 does not allow us to use the C++11 style attributes here. */
-/* clang 16.0.5 accepts the __attribute__ syntax here, but does not emit a symbol
- * for TemplateClass<A>::get() when annotated with
- * __attribute__((visibility("default"))*/
-/* gcc 13.1.1 allows C++11 style attributes here. */
-template class PUBLIC_ABI TemplateClass<A>;
 
 
 template <typename T> void TemplateClass<T>::get() { }
+
+/* clang 16.0.5 does not allow us to use the C++11 style attributes here. */
+/* clang 16.0.5 accepts the __attribute__ syntax here. */
+/* gcc 13.1.1 allows C++11 style attributes here. */
+
+// Note that in order for clang to emit a symbol for TemplateClass<A>::get(), the
+// explicit instantiation has to go after the get function definition.
+// gcc seems to generate the symbol no matter where it is.
+// See https://eel.is/c++draft/temp.explicit#11
+// See https://cplusplus.github.io/CWG/issues/470.html
+// See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=22263
+template class PUBLIC_ABI TemplateClass<A>;
