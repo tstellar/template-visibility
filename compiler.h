1f2bfac6fbf6169811f380b6d224fb6a5f6ed486
Tom Stellard <tstellar@redhat.com>
2023-07-22 20:51:54 -0700

    Add windows tests

--- compiler.h
+++ compiler.h
@@ -4,10 +4,16 @@
 #define PUBLIC_ABI [[gnu::visibility("default")]]
 #elif defined(NO_VISIBILITY)
 #define PUBLIC_ABI
-#else
+#elif defined(__ELF__)
 #  ifdef __clang__
 #    define PUBLIC_ABI __attribute__((visibility("default")))
 #  else
 #    define PUBLIC_ABI [[gnu::visibility("default")]]
+#  endif
+#else
+#  if defined(template_visibility_EXPORTS)
+#    define PUBLIC_ABI __declspec(dllexport)
+#  else
+#    define PUBLIC_ABI __declspec(dllimport)
 #  endif
 #endif
